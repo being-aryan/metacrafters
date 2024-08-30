faf62f1a902ff5f34cc5b96a43e342f2d051c4ec
being-aryan <134589308+being-aryan@users.noreply.github.com>
2024-08-30 12:28:53 +0530

    Update project4.sol

--- eth+avax/module4/project4.sol
+++ eth+avax/module4/project4.sol
@@ -1,67 +1,65 @@
 // SPDX-License-Identifier: MIT
-
-/*
-Minting new tokens: The platform should be able to create new tokens and distribute them to players as rewards. Only the owner can mint tokens.
-Transferring tokens: Players should be able to transfer their tokens to others.
-Redeeming tokens: Players should be able to redeem their tokens for items in the in-game store.
-Checking token balance: Players should be able to check their token balance at any time.
-Burning tokens: Anyone should be able to burn tokens, that they own, that are no longer needed.
-*/
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-
-contract DegenToken is ERC20, Ownable(msg.sender) {
-   
-    struct store {
-        uint256 id;
-        string name;
-        uint256 price;
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+//0x5B38Da6a701c568545dCfcB03FcB875f56beddC4
+contract DegenToken is ERC20, Ownable, ERC20Burnable {
+    struct PlayerItems {
+        uint goldCoin;
+        uint silverCoin;
+        uint gem;
+        uint magicPotion;
     }
 
-    store[] public Store; 
+    mapping(address => PlayerItems) public playerItems;
 
-    constructor() ERC20("Degen", "DKN") {
-        Store.push(store(1, "grenade", 10));
-        Store.push(store(2, "landmine", 20));
-        Store.push(store(3, "pistol", 50));
+    // Numerical identifiers for items
+    uint constant GOLD_COIN = 1;
+    uint constant SILVER_COIN = 2;
+    uint constant GEM = 3;
+    uint constant MAGIC_POTION = 4;
+
+    // Events
+    event Minted(address indexed to, uint amount);
+    event TokensTransferred(address indexed from, address indexed to, uint amount);
+    event ItemRedeemed(address indexed player, uint itemId, uint price);
+    event TokensBurned(address indexed from, uint amount);
+
+    constructor() ERC20("Degen", "DGN") Ownable(msg.sender) {}
+
+    function mint(address _to, uint amt) external onlyOwner {
+        _mint(_to, amt);
+        emit Minted(_to, amt);
     }
 
-    modifier check(uint256 amount){
-        require(amount > 0, "Amount must be greater than 0");
-        require(balanceOf(msg.sender) >= amount, "Insufficient balance");
-        _;
+// 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
+    function transferTokens(address _to, uint amt) public {
+        require(amt <= balanceOf(msg.sender), "Insufficient balance");
+        _transfer(msg.sender, _to, amt);
+        emit TokensTransferred(msg.sender, _to, amt);
     }
 
-    function decimals() override public pure returns (uint8){
-        return 0;
+    function redeemItem(uint _itemId, uint _price) public {
+        require(_itemId >= GOLD_COIN && _itemId <= MAGIC_POTION, "Invalid item ID");
+        require(balanceOf(msg.sender) >= _price, "Insufficient balance");
+
+        if (_itemId == GOLD_COIN) {
+            playerItems[msg.sender].goldCoin += 1;
+        } else if (_itemId == SILVER_COIN) {
+            playerItems[msg.sender].silverCoin += 1;
+        } else if (_itemId == GEM) {
+            playerItems[msg.sender].gem += 1;
+        } else if (_itemId == MAGIC_POTION) {
+            playerItems[msg.sender].magicPotion += 1;
+        }
+
+        _burn(msg.sender, _price);
+        emit ItemRedeemed(msg.sender, _itemId, _price);
     }
 
-    function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount);
+    function checkBalance() public view returns (uint) {
+        return balanceOf(msg.sender);
     }
-
-  
-    function transferTokens(address to, uint256 amount) public check(amount) {
-        _transfer(msg.sender, to, amount);
-    }   
-
-    function burnTokens(uint256 amount) public check(amount) {
-        _burn(msg.sender, amount);
-    }
-
-    function checkTokenBalance(address account) public view returns (uint256) {
-        return balanceOf(account);
-    }
-
-    function redeemTokens(uint256 itemId) public {
-        require(itemId < Store.length, "Invalid item ID");
-
-        store storage item = Store[itemId-1];
-        require(balanceOf(msg.sender) >= item.price, "Insufficient balance");
-
-        _burn(msg.sender, item.price);
-    }
-   
 }
